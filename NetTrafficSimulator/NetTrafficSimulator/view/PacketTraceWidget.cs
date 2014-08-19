206114cf167e329bbe250f5cceb801daa1fc28d8
Alexander Mansurov <alexander.mansurov@gmail.com>
2014-08-19 07:19:41 +0200

    PacketTraceWidget

---
+++ NetTrafficSimulator/NetTrafficSimulator/view/PacketTraceWidget.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace NetTrafficSimulator
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class PacketTraceWidget : Gtk.Bin
+	{
+		public PacketTraceWidget ()
+		{
+			this.Build ();
+		}
+	}
+}
+
