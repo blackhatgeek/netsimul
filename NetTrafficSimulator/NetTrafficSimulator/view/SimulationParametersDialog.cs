7c22401323ef1d3ef73a6d86b1e9bc352b65d3e1
Alexander Mansurov <alexander.mansurov@gmail.com>
2014-08-20 17:44:07 +0200

    Simulation parameters dialog

---
+++ NetTrafficSimulator/NetTrafficSimulator/view/SimulationParametersDialog.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace NetTrafficSimulator
+{
+	public partial class SimulationParametersDialog : Gtk.Dialog
+	{
+		//private SimulationModel sm;
+		public int maxHop,time;
+		public SimulationParametersDialog (SimulationModel sm)
+		{
+			//this.sm = sm;
+			this.maxHop = sm.MaxHop;
+			this.time = sm.Time;
+			this.Build ();
+			this.spinbutton1.Value = sm.MaxHop;
+			this.spinbutton2.Value = sm.Time;
+		}
+
+		protected void btnOKClicked (object sender, EventArgs e)
+		{
+			maxHop = spinbutton1.ValueAsInt;
+			time = spinbutton2.ValueAsInt;
+		}
+	}
+}
+
